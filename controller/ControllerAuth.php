530a9361bd6720f0d8ca0fe393eee72a411972ce
Jérémy <nicolletjeremy@gmail.com>
2025-07-21 16:03:42 +0200

    jsp

--- controller/ControllerAuth.php
+++ controller/ControllerAuth.php
@@ -1,35 +1,62 @@
 <?php
 class ControllerAuth {
-    public function register(){
-        if($_SERVER['REQUEST_METHOD'] === 'POST'){
-            if(empty($_POST['pseudo']) || empty($_POST['email']) || empty($_POST['password'])){
-                $_SESSION['error'] = "Tous les champs doivent être remplis !";
-                header('Location: /mangatheque/register');
-                exit;
-            }
-            $pseudo = trim($_POST['pseudo']); // Corrected line
-            $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
-            $password = trim($_POST['password']); // You might also want to trim the password
+        public function register(){
+            if($_SERVER['REQUEST_METHOD'] === 'POST'){
+                if(empty($_POST['pseudo']) || empty($_POST['email']) || empty($_POST['password'])){
+                    $_SESSION['error'] = "Tous les champs doivent être remplis !";
+                    header('Location: /mangatheque/register');
+                    exit;
+                }
 
-            $modelUser = new ModelUser();
-            $successUser = $modelUser->createUser($pseudo, $email, $password);
-            
-            if($successUser){
-                $_SESSION['success'] = "Vous êtes bien enregistré ! Vous pouvez vous connecter !";
-                header('Location: /mangatheque/login');
-                exit;
-            } else {
-                $_SESSION['error'] = "Erreur lors de l'insertion";
-                header('Location: /mangatheque/register');
-                exit;
+                $pseudo = trim($_POST['pseudo']);
+                $email = filter_var(trim($_POST['email']), FILTER_SANITIZE_EMAIL);
+                $password = trim($_POST['password']);
+
+                $modelUser = new ModelUser();
+                $successUser = $modelUser->createUser($pseudo, $email, $password);
+
+                if($successUser){
+                    $_SESSION['success'] = "Vous êtes bien enregistré ! Vous pouvez vous connecter !";
+                    header('Location: /mangatheque/login');
+                    exit;
+                } else {
+                    $_SESSION['error'] = "Erreur lors de l'insertion.";
+                    header('Location: /mangatheque/register');
+                    exit;
+                }
             }
+
+            require __DIR__ . '/../view/auth/register.php';
         }
 
-        require __DIR__ . '/../view/auth/register.php';
-    }
+        public function login(){
+            if($_SERVER['REQUEST_METHOD'] === 'POST'){
+                if(!empty($_POST['email']) && !empty($_POST['password'])){
+                    $modelUser = new ModelUser();
+                    $userSuccess = $modelUser->getUserByEmail($_POST['email']);
 
-    public function login(){
-        require __DIR__ . '/../view/auth/login.php';
-        
-    }
+                    if($userSuccess && password_verify($_POST['password'], $userSuccess->getPassword())){
+                        $_SESSION['success'] = 'Connexion réussie !';
+                        $_SESSION['id'] = $userSuccess->getId();
+                        $_SESSION['pseudo'] = $userSuccess->getPseudo();
+
+                        header('Location: /mangatheque/');
+                        exit;
+                    } else {
+                        $_SESSION['error'] = 'Identifiants incorrectes';
+
+                        require __DIR__ . '/../view/auth/login.php';
+                        exit;
+                    }
+                }
+            }
+
+            require __DIR__ . '/../view/auth/login.php';
+        }
+
+        public function logout(){
+             session_unset();
+             session_destroy();
+             header('Location: /mangatheque/login');
+        }
 }
--- controller/ControllerPage.php
+++ controller/ControllerPage.php
@@ -1,6 +1,10 @@
 <?php
 class ControllerPage {
     public function homePage(){
+        if(!isset($_SESSION['id'])){
+            header('Location: /mangatheque/login/');
+        }
+        
         $modelUser = new ModelUser();
         $users = $modelUser->getUsers();
         require './view/page/homepage.php';
